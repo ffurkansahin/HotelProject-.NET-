7f4eca55d514ea0426fcd9c836c1234d2c014cb1
furka <furkan.sahinbusiness@gmail.com>
2023-11-22 22:30:24 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class ServiceManager : IServiceService
+    {
+        private readonly IServicesDAL _servicesDAL;
+
+        public ServiceManager(IServicesDAL servicesDAL)
+        {
+            _servicesDAL = servicesDAL;
+        }
+
+        public void TDelete(Service t)
+        {
+            _servicesDAL.Delete(t);
+        }
+
+        public Service TGetByID(int id)
+        {
+            return _servicesDAL.GetByID(id);
+        }
+
+        public List<Service> TGetList()
+        {
+            return _servicesDAL.GetList();
+        }
+
+        public void TInsert(Service t)
+        {
+            _servicesDAL.Insert(t);
+        }
+
+        public void TUpdate(Service t)
+        {
+            _servicesDAL.Update(t);
+        }
+    }
+}
