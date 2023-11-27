9d54fc40c3dd4418b94f03b83d0cf3f8dcdb0d1f
furka <furkan.sahinbusiness@gmail.com>
2023-11-27 12:35:48 +0300

    Tested Json Web Token(JWT)

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult CreateToken()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+        [HttpGet("[action]")]
+        public IActionResult CreateTokenForAdmin()
+        {
+            return Ok(new CreateToken().TokenCreateForAdmin());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Hoşgeldiniz");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token başarılı bir şekilde giriş yaptı");
+        }
+    }
+}
