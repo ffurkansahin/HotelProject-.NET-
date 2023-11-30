0ce865277d59d5be5861dfc60d8ac43a0094aa6e
furka <furkan.sahinbusiness@gmail.com>
2023-11-30 19:28:56 +0300

    Added upload file and image part for api through frontend

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent content = new ByteArrayContent(bytes);
+            content.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(content, "file", file.FileName);
+
+            var client = new HttpClient();
+            await client.PostAsync("http://localhost:5035/api/FileImage", multipartFormDataContent);
+
+            return View();
+
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "furkann3371@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User",model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject=model.Subject;
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("furkann3371@gmail.com", "xhhvysqdjpvizcat");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            //Gönderilen mailin veritabanına kaydedilmesi
+
+            return View();
+        }
+    }
+}
