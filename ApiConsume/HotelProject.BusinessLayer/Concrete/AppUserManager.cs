6b7ea8644fd3e907726d8afdcab047a4885f9ede
furka <furkan.sahinbusiness@gmail.com>
2023-12-01 23:33:52 +0300

    Added AppUser by their locations part and fixed api error(refference loop)

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/AppUserManager.cs
@@ -0,0 +1,51 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class AppUserManager : IAppUserService
+    {
+        private readonly IAppUserDAL _appUserDAL;
+
+        public AppUserManager(IAppUserDAL appUserDAL)
+        {
+            _appUserDAL = appUserDAL;
+        }
+
+        public void TDelete(AppUser t)
+        {
+            _appUserDAL.Delete(t);
+        }
+
+        public AppUser TGetByID(int id)
+        {
+            return _appUserDAL.GetByID(id);
+        }
+
+        public List<AppUser> TGetList()
+        {
+            return _appUserDAL.GetList();
+        }
+
+        public void TInsert(AppUser t)
+        {
+            _appUserDAL.Insert(t);
+        }
+
+        public void TUpdate(AppUser t)
+        {
+            _appUserDAL.Update(t);
+        }
+
+        public List<AppUser> TUserListWithWorkLocation()
+        {
+            return _appUserDAL.UserListWithWorkLocation();
+        }
+    }
+}
--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -3,6 +3,7 @@ using HotelProject.BusinessLayer.Concrete;
 using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.EntityLayer.Concrete;
 
 namespace HotelProject.WebApi
 {
@@ -51,6 +52,8 @@ namespace HotelProject.WebApi
             builder.Services.AddScoped<IWorkLocationService, WorkLocationManager>();
             builder.Services.AddScoped<IWorkLocationDAL, EfWorkLocationDAL>();
 
+            builder.Services.AddScoped<IAppUserService, AppUserManager>();
+            builder.Services.AddScoped<IAppUserDAL, EfAppUserDAL>();
 
 
             builder.Services.AddAutoMapper(typeof(Program));
@@ -63,7 +66,7 @@ namespace HotelProject.WebApi
                 });
             });
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddNewtonsoftJson(options=>options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
